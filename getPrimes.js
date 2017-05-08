49c1071ca1f6a87d75dedfc2c04a9b3be7927c70
Dbrandy <dbrandydude@gmail.com>
2017-05-08 21:32:57 +0100

    Update on getPrimes.js - prime check on n

--- getPrimes.js
+++ getPrimes.js
@@ -1,4 +1,11 @@
 function getPrimes(n) {
     let prime = [], primes = [];
+    for (let counter = 2; counter <= n; ++counter) {
+        if (!prime[counter]) {
+            // if n is a prime number, add n to primes
+            primes.push(counter);
 
+        }
+    }
+    return primes;
 }
